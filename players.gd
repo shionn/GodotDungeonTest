7141409e4cb650b01bafe83a2b02b12e8b19b155
Shionn <shionn@gmail.com>
2026-06-02 13:59:18 +0200

    Test camero fps

--- players.gd
+++ players.gd
@@ -2,12 +2,19 @@ extends CharacterBody3D
 
 const RAY_LENGTH = 20.0
 
-@onready var _camera = $"../Camera3D" as Camera3D
+@onready var _camera = $Camera3D as Camera3D
 @onready var _floor = $"../Floor" as Node3D
+@onready var _head = $Character/Rig/Skeleton3D/Mage_Head as MeshInstance3D
+@onready var _headBNone = $Character/Rig/Skeleton3D/head as BoneAttachment3D
 
+
+func _ready() -> void:
+	_head.visible = false
+	
+	
 
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_vector("move_left", "move_righ", "move_front", "move_back")
+	#var input_dir := Input.get_vector("move_left", "move_righ", "move_front", "move_back")
 	
 	_cast()
 	
@@ -22,8 +29,28 @@ func _cast() -> void:
 	if !result.is_empty() :
 		print(result)
 		if (result["collider"] as StaticBody3D).collision_layer & 256 : # layer 9 
-			self.position = result["position"]
+			print(result["position"])
 	
 	
 #func _unhandled_input(event: InputEvent) -> void:
 	#print(event)
+const tilt_limit = deg_to_rad(50)
+const mouse_sensitivity = 0.01
+
+var _previous_mouse_position : Vector2
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and Input.is_action_just_pressed("move_camera"):
+		_previous_mouse_position = (event as InputEventMouseButton).position
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event is InputEventMouseButton and Input.is_action_just_released("move_camera") :
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		Input.warp_mouse(_previous_mouse_position)
+	elif event is InputEventMouseMotion and Input.is_action_pressed("move_camera"):
+		_camera.rotation.x -= event.relative.y * mouse_sensitivity
+		_camera.rotation.x = clampf(_camera.rotation.x, -tilt_limit, tilt_limit)
+		_camera.rotation.y += -event.relative.x * mouse_sensitivity
+		_headBNone.rotation.x = -_camera.rotation.x 
+		_headBNone.rotation.y = _camera.rotation.y 
+		#_headBNone.rotation.x += event.relative.y * mouse_sensitivity
+		#_headBNone.rotation.y += -event.relative.x * mouse_sensitivity
